84c2705327106caaf456f67e2e72afaf16644f25
Littleor <me@littleor.cn>
2025-05-21 19:34:24 +0800

    fix: version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="torch-module-cache",
-    version="0.1.",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
         "torch>=1.7.0",
